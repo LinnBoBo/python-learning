0efe07ee8ff9b8d9da0ee050ec423fea362f46c4
Linn Bo <133071384+LinnBoBo@users.noreply.github.com>
2023-10-18 21:01:52 +0630

    Add files via upload

---
+++ boolean.py
@@ -0,0 +1,16 @@
+#compression operators >, <, >=, <=, ==,!=
+score = 50
+high_score = 80
+print(score > high_score )
+print(score < high_score )
+print(score >= high_score)
+print(score <= high_score)
+print(score == high_score)
+print(score != high_score)
+
+#logical operators (and,or,not)
+print(score < high_score) and print(score <= high_score)
+print(score > high_score) and print(score < high_score)
+
+print(score == high_score) or (score != high_score)
+print(score == high_score) or (score > high_score)
