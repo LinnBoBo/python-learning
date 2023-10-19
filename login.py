ebb659f8a52b32ead3a569d24c8428537e82c340
Linn Bo <133071384+LinnBoBo@users.noreply.github.com>
2023-10-19 20:49:22 +0630

    Update

---
+++ login.py
@@ -0,0 +1,14 @@
+correct_username = "Linn"
+correct_pw = "2468ab"
+
+user_name = input('username: ')
+password =input('password: ')
+
+if user_name == correct_username and password == correct_pw:
+    print("You have Login successfully!")
+elif user_name != correct_username and password == correct_pw:
+    print("Worong username. Plz, try again!")
+elif user_name == correct_username and password != correct_pw:
+    print("Worong password. Plz, try again!") 
+elif user_name != correct_username and password != correct_pw:
+    print("Worng username and password! Plz, try again")
