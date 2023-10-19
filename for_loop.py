3358e329c9ee1cdd9c6f228a1c13e847f499763a
Linn Bo <133071384+LinnBoBo@users.noreply.github.com>
2023-10-19 20:41:57 +0630

    Add files via upload

---
+++ for_loop.py
@@ -0,0 +1,11 @@
+names = ['Tristan','Toly','Amarni','Monke']
+for i in names:
+    if i == 'Toly':
+        print(f'{i} is founder')
+        break   
+    else:
+        print(f'{i} is develper') 
+    
+
+
+ 
