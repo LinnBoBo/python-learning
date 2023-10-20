3037e5a2e9ba98ae287562290f69911ab83effa3
Linn Bo <133071384+LinnBoBo@users.noreply.github.com>
2023-10-20 07:46:36 +0630

    Update ranges.py

--- ranges.py
+++ ranges.py
@@ -2,7 +2,10 @@
 #for i in nums:
     #print(i)
 #for i in range(5):
-    #print('hello world!')    
+    #print('hello world!') 
+#for i in range(0,4,2): #0 is inclusive,
+    #print(i)           #4 is exclusive,
+                        #2 is prameter   
     
 
 fruits = ['apple','orange','banana','water melon','mango']
