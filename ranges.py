19426fb6e0966a7d44d3dbcc840d150a98d13a5e
Linn Bo <133071384+LinnBoBo@users.noreply.github.com>
2023-10-20 07:32:52 +0630

    Update

---
+++ ranges.py
@@ -0,0 +1,11 @@
+#nums = [1,2,3,4]
+#for i in nums:
+    #print(i)
+#for i in range(5):
+    #print('hello world!')    
+    
+
+fruits = ['apple','orange','banana','water melon','mango']
+ 
+for i in range(len(fruits)):
+    print(fruits[i])
