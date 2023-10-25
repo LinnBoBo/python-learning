356db4395c7ec76cfc04109ba9784b5dd553891b
Linn Bo <133071384+LinnBoBo@users.noreply.github.com>
2023-10-26 00:44:42 +0630

    Update

---
+++ dictionary.py
@@ -0,0 +1,20 @@
+person = { 'name' :'Yuuta',
+           'age' :20
+}
+
+#print(person['name'])
+
+#person['hair_color'] = "black"
+#print(person)
+
+#if 'name' in person:
+  # print('work')
+
+#if 'nothing' in person:
+#   print('work')
+
+#person_keys = list(person.keys())
+#print(person_keys)
+
+#person_values = list(person.values())
+#print(person_values)
